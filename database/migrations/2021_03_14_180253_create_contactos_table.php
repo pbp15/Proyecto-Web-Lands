c8c35191c0d3958a1600addd083d302cd7ba1888
joserb <rodriguezbjose1@gmail.com>
2021-03-14 13:22:50 -0500

    tablas contactos formularios

---
+++ database/migrations/2021_03_14_180253_create_contactos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('descripcion_ls');
+            $table->string('email', 60);
+            $table->string('telefono', 11);
+            $table->string('link_fb');
+            $table->string('link_in');
+            $table->string('link_msg');
+            $table->string('link_wsp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
---
+++ database/migrations/2021_03_14_181454_create_formularios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormulariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formularios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres', 80);
+            $table->string('email', 60);
+            $table->string('asunto');
+            $table->enum('tipo_servicio', ['pagina_web', 'sistema']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formularios');
+    }
+}
