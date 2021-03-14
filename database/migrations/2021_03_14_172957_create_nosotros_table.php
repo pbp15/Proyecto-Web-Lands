724bc0a41c92c23ce23faa9a0ec923d30c74ebd1
frank serrano basilio <frankserranobasilio@gmail.com>
2021-03-14 12:33:46 -0500

    Migracion tabla nosotros

---
+++ database/migrations/2021_03_14_172957_create_nosotros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNosotrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nosotros', function (Blueprint $table) {
+            $table->id();
+            $table->longText('despcripcion');
+            $table->longText('vision');
+            $table->longText('mision');
+            $table->longText('servicios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nosotros');
+    }
+}
